87e41b51a2dc4c3be8a73ba92588e7b6d154cbdd
Juan Pablo Vercesi <jpvercesi@tendrilinc.com>
2013-01-16 18:22:50 -0300

    Added parameters and default values for SlotController index action

--- server/app/controllers/slots_controller.rb
+++ server/app/controllers/slots_controller.rb
@@ -2,9 +2,9 @@ class SlotsController < ApplicationController
   def index
 
     # TODO: use defaults or specified
-    start_time = Time.now
-    end_time = start_time + 3.hour
-    duration = 1.hour
+    start_time = (params[:start_time] && Time.parse(params[:start_time])) || Time.now
+    end_time = (params[:end_time] && Time.parse(params[:end_time])) || start_time + 2.hour
+    duration = (params[:duration] && params[:duration].to_i.hours) || 1.hour
 
     slots = SlotRange.new(start_time, end_time, duration)
 
